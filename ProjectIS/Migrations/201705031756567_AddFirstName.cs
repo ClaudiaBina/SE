4d5fe003b8093a55f6dd316a286f336715af2a93
AlexSpinu <alexx.spinu@yahoo.com>
2017-05-03 23:24:14 +0300

    First look -login IS

---
+++ ProjectIS/Migrations/201705031756567_AddFirstName.cs
@@ -0,0 +1,18 @@
+namespace ProjectIS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddFirstName : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "FirstName", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "FirstName");
+        }
+    }
+}
---
+++ ProjectIS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectIS.Startup))]
+namespace ProjectIS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
